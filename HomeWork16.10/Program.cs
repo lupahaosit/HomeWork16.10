b4228e4eeb5c66a456b326be5926b9869aa15338
leman <igor2002vlad@mail.ru>
2022-06-04 20:57:09 +0500

    Final

--- HomeWork16.10/Program.cs
+++ HomeWork16.10/Program.cs
@@ -9,28 +9,34 @@ namespace HomeWork16._10
         static void Method()
         {
             int secondThread = Thread.CurrentThread.ManagedThreadId;
+            var th = Thread.CurrentThread;
+            th.Name = "SecondThread";
             Console.WriteLine($"начал работу Thread id = {secondThread}");
             for (int i = 0; i < 50; i++)
             {
                 Console.WriteLine($"{i}- ");
                 Thread.Sleep(200);
             }
-            Console.WriteLine($"Thread id = {secondThread} закончил работу");
+            Console.WriteLine($"{th.Name}, id = {secondThread} закончил работу");
         }
         static void Method2()
         {
             int ThirdThread = Thread.CurrentThread.ManagedThreadId;
+            var th = Thread.CurrentThread;
+            th.Name = "ThirdThread";
             Console.WriteLine($"начал работу Thread id = {ThirdThread}");
             for (int i = 0; i < 25; i++)
             {
                 Console.WriteLine($"{i}+ ");
                 Thread.Sleep(250);
             }
-            Console.WriteLine($"Thread id = {ThirdThread} закончил работу");
+            Console.WriteLine($"{th.Name}, id = {ThirdThread} закончил работу");
         }
         static void Main(string[] args)
         {
             Console.WriteLine($"основной поток id = {Thread.CurrentThread.ManagedThreadId}");
+            var th = Thread.CurrentThread;
+            th.Name = "MainThread";
             Task t1 = new Task(Method);
             Task t2 = new Task(Method2);
             t1.Start();
@@ -41,7 +47,7 @@ namespace HomeWork16._10
                 Thread.Sleep(500);
 
             }
-            Console.WriteLine($"Thread id = {Thread.CurrentThread.ManagedThreadId} закончил работу");
+            Console.WriteLine($"{th.Name}, id = {Thread.CurrentThread.ManagedThreadId} закончил работу");
 
             Task.WaitAll(t1, t2);
 
